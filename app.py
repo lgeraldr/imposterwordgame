d18d3a98b8b782dc0419dde177571c1036bfa787
lgeraldr <laughlanroche.pei@gmail.com>
2025-12-19 19:43:45 -0400

    app.py

--- app.py
+++ app.py
@@ -69,7 +69,11 @@ def final():
     return render_template("final.html", game=game)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
